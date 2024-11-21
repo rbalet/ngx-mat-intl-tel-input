2dab1ea484a8e3c959629866fe1b5e3144f3edf2
Christian Loosli <christian.loosli@vgt.energy>
2024-11-21 11:47:55 +0100

    feat(updateErrorState): updateErrorState

--- projects/ngx-mat-input-tel/src/lib/ngx-mat-input-tel.component.ts
+++ projects/ngx-mat-input-tel/src/lib/ngx-mat-input-tel.component.ts
@@ -19,6 +19,7 @@ import {
   booleanAttribute,
 } from '@angular/core'
 import {
+  FormControl,
   FormGroupDirective,
   FormsModule,
   NG_VALIDATORS,
@@ -61,10 +62,6 @@ class ngxMatInputTelBase {
   ) {}
 }
 
-const _ngxMatInputTelMixinBase: typeof ngxMatInputTelBase = mixinErrorState(
-  ngxMatInputTelBase as _AbstractConstructor<any>,
-)
-
 @Component({
   standalone: true,
   selector: 'ngx-mat-input-tel',
@@ -98,7 +95,7 @@ const _ngxMatInputTelMixinBase: typeof ngxMatInputTelBase = mixinErrorState(
   ],
 })
 export class NgxMatInputTelComponent
-  extends _ngxMatInputTelMixinBase
+  extends ngxMatInputTelBase
   implements OnInit, DoCheck, OnDestroy
 {
   static nextId = 0
@@ -229,6 +226,16 @@ export class NgxMatInputTelComponent
 
     this._changeDetectorRef.markForCheck()
     this.stateChanges.next()
+  }
+
+  updateErrorState() {
+    if (this.ngControl && this.ngControl.invalid && (this.ngControl.touched || (this._parentForm && this._parentForm.submitted))) {
+      const currentState = this.errorStateMatcher.isErrorState(this.ngControl.control as FormControl, this.ngControl?.value);
+      if (currentState !== this.errorState) {
+        this.errorState = currentState;
+        this._changeDetectorRef.markForCheck();
+      }
+    }
   }
 
   private _setDefaultCountry() {
